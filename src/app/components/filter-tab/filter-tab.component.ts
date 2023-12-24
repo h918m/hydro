9c9fa92f401841d3ba7431b7afc5163a9b727079
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-24 19:24:49 +0200

    finished the filter tab

--- src/app/components/filter-tab/filter-tab.component.ts
+++ src/app/components/filter-tab/filter-tab.component.ts
@@ -1,16 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-filter-tab',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      filter-tab works!
-    </p>
-  `,
-  styleUrl: './filter-tab.component.css'
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatExpansionModule,
+    FormsModule,
+    MatCheckboxModule,
+  ],
+  templateUrl: 'filter-tab.component.html',
+  styleUrl: './filter-tab.component.css',
 })
 export class FilterTabComponent {
-
+  controls = [
+    {
+      title: 'Permit & License',
+      checks: [
+        'Driller Permit',
+        'Borehole Drill Permit',
+        'Microsoft',
+        'MSI',
+        'Lenovo',
+      ],
+    },
+    {
+      title: 'Processor',
+      checks: ['Intel', 'AMD'],
+    },
+    {
+      title: 'Harddisk Type',
+      checks: ['SSD', 'HDD'],
+    },
+    {
+      title: 'Harddisk Størrelse',
+      checks: ['500 GB', '1 TB', '2 TB'],
+    },
+  ];
 }
